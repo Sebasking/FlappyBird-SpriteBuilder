93c62208264933c41b982321fcb751776bbda3b8
Sebasking <Sebasking@gmail.com>
2014-12-29 15:33:38 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -19,7 +19,7 @@
     // put update code here
     timeSinceObstacle += delta;
     
-    if(timeSinceObstacle > 0.4f){
+    if(timeSinceObstacle > 2.0f){
         
         [self addObstacle];
         
